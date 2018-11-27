5e11a746b50a8156944ef811a2893b0de6e42a92
Utkarsh Agarwal <35272322+utkarsh2019@users.noreply.github.com>
2018-11-27 17:53:47 -0500

    Few fixes

--- app/src/main/java/com/postplaylist/postplaylist/MainActivity.java
+++ app/src/main/java/com/postplaylist/postplaylist/MainActivity.java
@@ -55,7 +55,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
-        posts = new ArrayList<PostItem>();
+        posts = new ArrayList<  PostItem>();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
